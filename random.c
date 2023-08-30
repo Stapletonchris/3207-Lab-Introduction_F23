e0f118e9d0c396502c9846c4640220e752abdbea
Christopher Christmas <cchristmas21@Christophers-MacBook-Pro.local>
2023-08-30 14:00:51 -0400

    Added randomchar function and logic

---
+++ random.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char randomchar();
+
+int main(){
+
+}
+
+char randomchar(){
+    int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
+}
